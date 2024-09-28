85ce7eb26b8a257896a694e7078e9efb7df86148
sahar2710 <sahar.convent@gmail.com>
2024-09-28 16:52:45 -0700

    commit #3

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,9 +7,12 @@ kernal_data = np.array(img)
 edge_detection_matrix = np.array(([0,-1,0], 
                                  [-1, 8, -1],
                                  [-1, -1, -1]))
+final = np.array((0))
 x = np.array((3,3))
-for i in range(200):
+'''for i in range(200):
     for j in range(200):
         x = np.dot(kernal_data[i,j], edge_detection_matrix)
-        print(x)
-
+        for k in range(3):
+            final
+'''
+print(edge_detection_matrix)
